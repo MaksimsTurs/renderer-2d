92d795e1d92962d18c719253bc169cf1c8f2e663
MaksimsTurs <maksims07turs@gmail.com>
2026-07-03 12:42:31 +0200

    feat: add prototype of camera2d

---
+++ camera2d.h
@@ -0,0 +1,11 @@
+#ifndef H_RENDERER2D_CAMERA2D
+#define H_RENDERER2D_CAMERA2D
+
+#include "matrix.h"
+#include "transform2d.h"
+
+typedef struct {
+  transform2d_t transform;
+} camera2d_t;
+
+#endif
