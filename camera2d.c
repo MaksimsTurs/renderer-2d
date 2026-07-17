d8398f4e6e40a769f3298e37c71c9915106b3563
MaksimsTurs <maksims07turs@gmail.com>
2026-07-17 11:27:14 +0200

    refactor: remove reduntan 2d camera implementation

--- camera2d.c
+++
@@ -1,9 +0,0 @@
-#include "include/camera2d.h"
-
-camera2d_t camera2d_create(f32_t left, f32_t right, f32_t top, f32_t bottom)
-{
-  return(camera2d_t){
-    {{ 0.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 1.0f }, 0.0f },
-    mat3x3f32_orthographic(left, right, top, bottom),
-  };
-}
